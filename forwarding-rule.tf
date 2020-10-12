d858e13aa93d190c9767a726e29f0f8013f66e23
Gene Trog <gtrog@renttherunway.com>
2020-10-12 12:31:46 -0400

    try fix

--- forwarding-rule.tf
+++ forwarding-rule.tf
@@ -14,21 +14,11 @@
  * limitations under the License.
  */
 
-locals {
-  vpn_gw_ip = var.vpn_gw_ip == "" ? google_compute_address.vpn_gw_ip[0].address : var.vpn_gw_ip
-}
-
-resource "null_resource" "debug" {
-  provisioner "local-exec" {
-    command = "echo ip: ${var.vpn_gw_ip}"
-  }
-}
-
 # Assosciate external IP/Port-range to VPN-GW by using Forwarding rules
 resource "google_compute_forwarding_rule" "vpn_esp" {
   name        = "${google_compute_vpn_gateway.vpn_gateway.name}-esp"
   ip_protocol = "ESP"
-  ip_address  = local.vpn_gw_ip
+  ip_address  = var.vpn_gw_ip == "" ? google_compute_address.vpn_gw_ip[0].address : var.vpn_gw_ip
   target      = google_compute_vpn_gateway.vpn_gateway.self_link
   project     = var.project_id
   region      = var.region
@@ -38,7 +28,7 @@ resource "google_compute_forwarding_rule" "vpn_udp500" {
   name        = "${google_compute_vpn_gateway.vpn_gateway.name}-udp500"
   ip_protocol = "UDP"
   port_range  = "500"
-  ip_address  = local.vpn_gw_ip
+  ip_address  = var.vpn_gw_ip == "" ? google_compute_address.vpn_gw_ip[0].address : var.vpn_gw_ip
   target      = google_compute_vpn_gateway.vpn_gateway.self_link
   project     = var.project_id
   region      = var.region
@@ -48,7 +38,7 @@ resource "google_compute_forwarding_rule" "vpn_udp4500" {
   name        = "${google_compute_vpn_gateway.vpn_gateway.name}-udp4500"
   ip_protocol = "UDP"
   port_range  = "4500"
-  ip_address  = local.vpn_gw_ip
+  ip_address  = var.vpn_gw_ip == "" ? google_compute_address.vpn_gw_ip[0].address : var.vpn_gw_ip
   target      = google_compute_vpn_gateway.vpn_gateway.self_link
   project     = var.project_id
   region      = var.region
